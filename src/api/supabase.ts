7e66fe6c3d0a68063008393af7cc5d26e49fb20e
MarcinK <marcin.kulbicki@gmail.com>
2024-08-05 16:51:07 +0200

    chore(api): add api key directly to file

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const supabaseUrl = 'https://ooszicfynlpqvpfwdunb.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vc3ppY2Z5bmxwcXZwZndkdW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2MDg3NzksImV4cCI6MjAzODE4NDc3OX0.3cF8aUZPidDeganMnmigNWZHEp8RHIBbWCvQvnP80oU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
