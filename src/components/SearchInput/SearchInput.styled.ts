f51dc563ba515be2a510d1e1eb01edf2eec1be94
MarcinK <marcin.kulbicki@gmail.com>
2024-08-14 19:21:40 +0200

    styles(SearchInput): move styles to a seperate file

---
+++ src/components/SearchInput/SearchInput.styled.ts
@@ -0,0 +1,61 @@
+import styled, { css } from 'styled-components';
+import { device } from '../../styles/breakpoints';
+
+interface StyledInputWrapperProps {
+    type?: 'header';
+}
+
+export const StyledInputWrapper = styled.div<StyledInputWrapperProps>`
+    width: 95vw;
+    margin: 0 auto;
+    display: block;
+    padding: ${props => (props.type !== 'header' ? '0 0 1rem 0' : '1rem 0')};
+    max-width: 40rem;
+
+    @media ${device.tablet} {
+        ${props =>
+            props.type !== 'header' &&
+            css`
+                display: none;
+            `}
+    }
+`;
+
+export const StyledInputContainer = styled.div`
+    position: relative;
+`;
+
+export const StyledInput = styled.input`
+    width: 100%;
+    border-radius: 100vmax;
+    padding: 0 1rem 0 2.75rem;
+    border: 1px solid var(--color-grey-200);
+    height: 2.75rem;
+    transition: border 0.3s cubic-bezier(0.39, 0.575, 0.565, 1);
+    font-size: 0.9rem;
+    font-style: italic;
+    transition: border-color var(--animation-and-timing);
+
+    &:focus {
+        border-color: var(--color-black);
+    }
+`;
+
+export const StyledIconWrapper = styled.span`
+    position: absolute;
+    font-size: 18px;
+    height: 100%;
+    top: 50%;
+    left: 1rem;
+    transform: translate(0, -25%);
+`;
+
+export const StyledButton = styled.button`
+    position: absolute;
+    border: none;
+    background-color: transparent;
+    font-size: 1.5rem;
+    top: 50%;
+    transform: translate(0, -50%);
+    right: -3rem;
+`;
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,67 +1,13 @@
-import styled, { css } from 'styled-components';
 import { IoSearchOutline } from 'react-icons/io5';
-import { device } from '../../styles/breakpoints';
 import { LiaTimesSolid } from 'react-icons/lia';
-import { useSearchInputContext } from '../../contexts/SearchInputContext';
 
-interface StyledInputWrapperProps {
-    type?: 'header';
-}
-
-const StyledInputWrapper = styled.div<StyledInputWrapperProps>`
-    width: 95vw;
-    margin: 0 auto;
-    display: block;
-    padding: ${props => (props.type !== 'header' ? '0 0 1rem 0' : '1rem 0')};
-    max-width: 40rem;
-
-    @media ${device.tablet} {
-        ${props =>
-            props.type !== 'header' &&
-            css`
-                display: none;
-            `}
-    }
-`;
-
-const StyledInputContainer = styled.div`
-    position: relative;
-`;
-
-const StyledInput = styled.input`
-    width: 100%;
-    border-radius: 100vmax;
-    padding: 0 1rem 0 2.75rem;
-    border: 1px solid var(--color-grey-200);
-    height: 2.75rem;
-    transition: border 0.3s cubic-bezier(0.39, 0.575, 0.565, 1);
-    font-size: 0.9rem;
-    font-style: italic;
-    transition: border-color var(--animation-and-timing);
-
-    &:focus {
-        border-color: var(--color-black);
-    }
-`;
-
-const StyledIconWrapper = styled.span`
-    position: absolute;
-    font-size: 18px;
-    height: 100%;
-    top: 50%;
-    left: 1rem;
-    transform: translate(0, -25%);
-`;
-
-const StyledButton = styled.button`
-    position: absolute;
-    border: none;
-    background-color: transparent;
-    font-size: 1.5rem;
-    top: 50%;
-    transform: translate(0, -50%);
-    right: -3rem;
-`;
+import {
+    StyledButton,
+    StyledIconWrapper,
+    StyledInput,
+    StyledInputContainer,
+    StyledInputWrapper,
+} from './SearchInput.styled';
 
 interface SeachInputProps {
     type?: 'header';
