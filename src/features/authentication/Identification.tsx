ddb0c18b50446222ffcda721b6af127fd569305e
MarcinK <marcin.kulbicki@gmail.com>
2024-08-17 11:40:57 +0200

    test: test login functionality on production environment

--- src/features/authentication/Identification.tsx
+++ src/features/authentication/Identification.tsx
@@ -1,8 +1,8 @@
 import { Sidebar } from '../../components/Sidebar';
 import { useLoginModalContext } from '../../contexts/LoginModalContext';
 
-import CreateAccountForm from './CreateAccountForm';
-import LoginForm from './LoginModal';
+// import CreateAccountForm from './CreateAccountForm';
+// import LoginForm from './LoginModal';
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
@@ -10,11 +10,11 @@ const StyledWrapper = styled.div`
 `;
 
 function LoginModal() {
-    const { toggleLoginModal, toggleCreateAccountView, isCreateAccountView } = useLoginModalContext();
+    const { toggleLoginModal, toggleCreateAccountView, isCreateAccountView, isOpen } = useLoginModalContext();
 
     return (
-        <Sidebar toggleSidebarNavigation={toggleLoginModal} isOpen={true} slideFrom='right'>
-            <StyledWrapper>
+        <Sidebar toggleSidebarNavigation={toggleLoginModal} isOpen={isOpen} slideFrom='right'>
+            {/* <StyledWrapper>
                 {!isCreateAccountView ? (
                     <LoginForm toggleModal={toggleLoginModal} toggleCreateAccountView={toggleCreateAccountView} />
                 ) : (
@@ -22,6 +22,22 @@ function LoginModal() {
                         toggleCreateAccountView={toggleCreateAccountView}
                         toggleModal={toggleLoginModal}
                     />
+                )}
+            </StyledWrapper> */}
+
+            <StyledWrapper>
+                {!isCreateAccountView ? (
+                    <div>
+                        <h4>LoginView</h4>
+                        <hr />
+                        <p>No account?</p>
+                        <button onClick={toggleCreateAccountView}>Create an acccount</button>
+                    </div>
+                ) : (
+                    <div>
+                        <h4>Create account view</h4>
+                        <button onClick={toggleCreateAccountView}>go back to login</button>
+                    </div>
                 )}
             </StyledWrapper>
         </Sidebar>
