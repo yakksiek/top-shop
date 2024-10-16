f48c13a3c9f99f4488bbb18eb727b208da4fa4e6
MarcinK <marcin.kulbicki@gmail.com>
2024-10-16 11:57:09 +0200

    fix(FieldREnderer): fix error type

--- src/components/Form/FieldRenderer.tsx
+++ src/components/Form/FieldRenderer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import { FieldError, useFormContext } from 'react-hook-form';
 import FormRow from './FormRow';
 import { FieldConfig } from '../../db/PersonalInformationFormData';
 
@@ -17,8 +17,9 @@ const FieldRenderer: React.FC<FieldRendererProps> = ({ fieldName, fieldConfig })
     const isRequired = 'validation' in fieldConfig && fieldConfig.validation && 'required' in fieldConfig.validation;
 
     const Component = fieldConfig.component;
-    const errorMessage =
-        typeof errors[fieldName] === 'string' ? errors[fieldName] : (errors[fieldName]?.message as string);
+
+    const errorObject = errors[fieldName] as FieldError | undefined;
+    const errorMessage = errorObject?.message || '';
 
     return (
         <FormRow key={fieldName} label={fieldConfig.label} error={errorMessage} isRequired={isRequired}>
