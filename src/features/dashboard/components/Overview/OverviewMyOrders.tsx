ef924aabdf5ed112746e3ff27725b6e9f17c039f
MarcinK <marcin.kulbicki@gmail.com>
2024-10-16 17:37:01 +0200

    feat(OverviewMyOrders): add click handle on button

--- src/features/dashboard/components/Overview/OverviewMyOrders.tsx
+++ src/features/dashboard/components/Overview/OverviewMyOrders.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../../../../components/Button';
 
 function OverviewMyOrders() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    };
+
     return (
         <div>
             <p>There are no current orders</p>
-            <Button fill={true}>Start Shopping</Button>
+            <Button fill={true} onClick={handleClick}>
+                Start Shopping
+            </Button>
         </div>
     );
 }
